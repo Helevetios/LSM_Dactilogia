f38a8b9ba8bff45ce6b0bec7ad15980c809596f5
Juan <juaniak109@outlook.es>
2023-11-06 01:52:31 -0600

    ReconocedorLSM

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 import { HandLandmarker, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 const demosSection = document.getElementById("demos");
 const predictButton = document.getElementById('predict');
-const collectedData = document.getElementById('data_collect');
 let handLandmarker = undefined;
 let runningMode = "IMAGE";
 let enableWebcamButton;
 let webcamRunning = false;
+let modelCustom;
+let predict = false;
 
 const createHandLandmarker = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
@@ -40,6 +41,7 @@ else {
 }
 // Enable the live webcam view and start detection.
 function enableCam(event) {
+    predict = true;
     if (!handLandmarker) {
         console.log("Wait! objectDetector not loaded yet.");
         return;
@@ -51,7 +53,6 @@ function enableCam(event) {
     else {
         webcamRunning = true;
         predictButton.style.display = "block";
-        collectedData.style.display = "block"
         enableWebcamButton.remove();
     }
     // getUsermedia parameters.
@@ -67,8 +68,7 @@ function enableCam(event) {
         videoPlaying = true;
     });
 
-    const buttons = document.getElementById("buttons")
-    buttons.style.display = "inline";
+
 
     predictLoop()
 }
@@ -112,44 +112,21 @@ async function predictWebcam() {
 
 const STATUS = document.getElementById('status');
 const PREDICT = document.getElementById('predict');
-const RESET_BUTTON = document.getElementById('reset');
-const TRAIN_BUTTON = document.getElementById('train');
+PREDICT.innerText = 'Sin modelos para predecir';
 const MOBILE_NET_INPUT_WIDTH = 224;
 const MOBILE_NET_INPUT_HEIGHT = 224;
 const STOP_DATA_GATHER = -1;
-const CLASS_NAMES = [];
+let CLASS_NAMES = [];
 
-TRAIN_BUTTON.addEventListener('click', trainAndPredict);
-RESET_BUTTON.addEventListener('click', reset);
 
-let dataCollectorButtons = document.querySelectorAll('button.dataCollector');
-for (let i = 0; i < dataCollectorButtons.length; i++) {
-    dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass);
-    dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass);
-    // For mobile.
-    dataCollectorButtons[i].addEventListener('touchend', gatherDataForClass);
-
-    // Populate the human readable names for classes.
-    CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
-}
-
-function gatherDataForClass() {
-    let classNumber = parseInt(this.getAttribute('data-1hot'));
-    gatherDataState = (gatherDataState === STOP_DATA_GATHER) ? classNumber : STOP_DATA_GATHER;
-    dataGatherLoop();
-}
+CLASS_NAMES.push('0')
+CLASS_NAMES.push('1')
 
 
 let mobilenet = undefined;
 let gatherDataState = STOP_DATA_GATHER;
 let videoPlaying = false;
-let trainingDataInputs = [];
-let trainingDataOutputs = [];
-let examplesCount = [];
-let predict = false;
 
-const save = document.getElementById('guardar');
-save.addEventListener("click", saveModel)
 
 async function loadMobileNetFeatureModel() {
     const URL = 'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
@@ -185,7 +162,7 @@ function predictLoop() {
     if (predict) {
         tf.tidy(function () {
             let imageFeatures = calculateFeaturesOnCurrentFrame();
-            let prediction = model.predict(imageFeatures.expandDims()).squeeze();
+            let prediction = modelCustom.predict(imageFeatures.expandDims()).squeeze();
             let highestIndex = prediction.argMax().arraySync();
             let predictionArray = prediction.arraySync();
             PREDICT.innerText = 'Prediccion: ' + CLASS_NAMES[highestIndex] + ' con ' + Math.floor(predictionArray[highestIndex] * 100) + '% confianza';
@@ -198,7 +175,6 @@ function predictLoop() {
 function calculateFeaturesOnCurrentFrame() {
     return tf.tidy(function () {
         // Grab pixels from current VIDEO frame.
-        console.log
         let videoFrameAsTensor = tf.browser.fromPixels(canvasElement);
         // Resize video frame tensor to be 224 x 224 pixels which is needed by MobileNet for input.
         let resizedTensorFrame = tf.image.resizeBilinear(
@@ -213,76 +189,31 @@ function calculateFeaturesOnCurrentFrame() {
     });
 }
 
-async function trainAndPredict() {
-    predict = false;
-    tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
 
-    let outputsAsTensor = tf.tensor1d(trainingDataOutputs, 'int32');
-    let oneHotOutputs = tf.oneHot(outputsAsTensor, CLASS_NAMES.length);
-    let inputsAsTensor = tf.stack(trainingDataInputs);
-
-    let results = await model.fit(inputsAsTensor, oneHotOutputs, {
-        shuffle: true,
-        batchSize: 5,
-        epochs: 10,
-        callbacks: { onEpochEnd: logProgress }
-    });
-
-    outputsAsTensor.dispose();
-    oneHotOutputs.dispose();
-    inputsAsTensor.dispose();
-
-    predict = true;
-    predictLoop();
-}
-
-function reset() {
-    predict = false;
-    examplesCount.splice(0);
-    for (let i = 0; i < trainingDataInputs.length; i++) {
-        trainingDataInputs[i].dispose();
-    }
-    trainingDataInputs.splice(0);
-    trainingDataOutputs.splice(0);
-    collectedData.innerText = 'Sin datos';
-    predictButton.innerText = '';
-    console.log('Tensors in memory: ' + tf.memory().numTensors);
-}
-
-function dataGatherLoop() {
-    // Only gather data if webcam is on and a relevent button is pressed.
-    if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
-        // Ensure tensors are cleaned up.
-        let imageFeatures = calculateFeaturesOnCurrentFrame();
-        trainingDataInputs.push(imageFeatures);
-        trainingDataOutputs.push(gatherDataState);
-        //const img = canvasElement.toDataURL();
-        //console.log(imageFeatures)
-        // Intialize array index element if currently undefined.
-        if (examplesCount[gatherDataState] === undefined) {
-            examplesCount[gatherDataState] = 0;
-        }
-        // Increment counts of examples for user interface to show.
-        examplesCount[gatherDataState]++;
-
-
-        collectedData.innerText = '';
-
-        for (let n = 0; n < CLASS_NAMES.length; n++) {
-            collectedData.innerText += CLASS_NAMES[n] + ' cant. datos: ' + examplesCount[n] + ' . ';
-        }
-
-        window.requestAnimationFrame(dataGatherLoop);
-    }
-}
 
 function logProgress(epoch, logs) {
     console.log('Data for epoch ' + epoch, logs);
 }
 
 
+var dropdownMenu = document.querySelector('.dropdown-menu');
 
-async function saveModel() {
-    console.log(model)
-    await model.save('downloads://modelCustom')
+dropdownMenu.addEventListener('click', function (event) {
+    // Evitar que el enlace se comporte como un enlace normal
+    event.preventDefault();
+
+    // Obtener la opción seleccionada
+    var selectedOption = event.target.textContent;
+
+    load(selectedOption.toLowerCase());
+});
+
+async function load(option) {
+    let URL = '/models/' + option + '/'
+    modelCustom = await tf.loadLayersModel(URL + 'modelCustom.json');
+    modelCustom.summary();
+    CLASS_NAMES = []
+    CLASS_NAMES.push('Palma')
+    CLASS_NAMES.push(option.toUpperCase())
+    predictLoop()
 }
